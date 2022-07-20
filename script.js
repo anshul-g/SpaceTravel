0c138aed50409ab10631c9404bb3ceeee060c385
Anshul Gandhi <anshulgandhi2001@gmail.com>
2022-07-20 23:47:29 +0530

    Added comments

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 'use strict';
 
+// get all the tabs
 const tabValues = document.querySelectorAll('[data-tab-value]');
+
+// get all the elements that are going to be interactive
 const tabContent = document.querySelectorAll('[data-tab]');
 
+
 tabValues.forEach(tab => {
+    //for each tab added an event listener
     tab.addEventListener("click", () => {
         
         // for destination page only
+        // remove class "active" from all the parent nodes of the tabs
         for(let k=0; k<tabValues.length; k++){
             tabValues[k].parentElement.classList.remove("active");
         }
 
+        // then add the active class only to the current tab
         const parent = tab.parentElement;
         parent.classList.add("active");
 
         // for all pages
+        // set aria-selected = false for all the tabs
         for(let i=0; i<tabValues.length; i++){
             tabValues[i].setAttribute("aria-selected", "false");
         }
 
+        // add hidden attribute to all the interactive elements
+        // except for those that have the data-tab = data-tab-value of the current tab 
         for(let j=0; j<tabContent.length; j++){
             tabContent[j].setAttribute("hidden","");
             if(tabContent[j].dataset.tab == tab.dataset.tabValue){
@@ -26,6 +36,8 @@ tabValues.forEach(tab => {
             }
         }
 
+        // finally set aria-selected to true for the current tab
+        // so that it is highlighted
         tab.setAttribute("aria-selected", "true");
     })
 })
