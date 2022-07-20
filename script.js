66fde85c47b3c8edcf27b8ebc7d058325e3bb253
Anshul Gandhi <anshulgandhi2001@gmail.com>
2022-07-20 23:25:57 +0530

    Added interactivity

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 'use strict';
 
-// TECHNOLOGY
+const tabValues = document.querySelectorAll('[data-tab-value]');
+const tabContent = document.querySelectorAll('[data-tab]');
 
-const tabValues = document.querySelectorAll(['data-tab-value']);
-console.log(tabValues);
+tabValues.forEach(tab => {
+    tab.addEventListener("click", () => {
+        
+        // for destination page only
+        for(let k=0; k<tabValues.length; k++){
+            tabValues[k].parentElement.classList.remove("active");
+        }
+
+        const parent = tab.parentElement;
+        parent.classList.add("active");
+
+        // for all pages
+        for(let i=0; i<tabValues.length; i++){
+            tabValues[i].setAttribute("aria-selected", "false");
+        }
+
+        for(let j=0; j<tabContent.length; j++){
+            tabContent[j].setAttribute("hidden","");
+            if(tabContent[j].dataset.tab == tab.dataset.tabValue){
+                tabContent[j].removeAttribute("hidden");
+            }
+        }
+
+        tab.setAttribute("aria-selected", "true");
+    })
+})
